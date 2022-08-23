1d860da378eb86ed571e30d3671610bc2a076454
baeonette <baeonette@gmail.com>
2022-08-22 20:01:44 -0700

    Initial commit

---
+++ %command_name%.subcommand.ts
@@ -0,0 +1,7 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+async function %command_name%Subcommand(interaction) {
+    interaction.reply('%command_name% works!');
+}
+
+export = %command_name%Subcommand;
